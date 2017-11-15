44bddaa158e2ed8ac749508d090dc18cbcb1f57b
CristianG540 <cristianneatproy@gmail.com>
2017-11-15 16:02:57 -0500

    new client ui improvement

--- resources/views/show-order.blade.php
+++ resources/views/show-order.blade.php
@@ -31,8 +31,10 @@
                     @if( isset($orden->newClient) )
                     <div class="row well text-center">
                         Nuevo cliente
-                        <div class="col-md-6"><b>Nombre: </b> {{ $orden->newClient->nombre or 'No se ingreso' }} </div>
-                        <div class="col-md-6"><b>NIT: </b> {{ $orden->newClient->codCliente or 'No se ingreso' }}</div>
+                        <div class="row">
+                            <div class="col-md-6"><b>Nombre: </b> {{ $orden->newClient->nombre or 'No se ingreso' }} </div>
+                            <div class="col-md-6"><b>NIT: </b> {{ $orden->newClient->codCliente or 'No se ingreso' }}</div>
+                        </div>
                     </div>
                     @endif
                     @if( isset($orden->error) && $orden->error )
